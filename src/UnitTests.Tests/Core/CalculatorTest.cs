7aa8bf51807fd95707fd6b7e0e55d4b0cc71fe58
gcishevs <german.cishevskiy@gmail.com>
2013-01-24 21:15:12 +0200

    commit initila solution

---
+++ src/UnitTests.Tests/Core/CalculatorTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests.Core;
+
+namespace UnitTests.Tests.Core
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Check_Add_Without_Parameters()
+        {
+            Calculator calc = new Calculator();
+            var actual = calc.Add();
+            Assert.AreEqual(actual, 0);
+        }
+    }
+}
---
+++ src/UnitTests/Core/Calculator.cs
@@ -0,0 +1,12 @@
+﻿
+namespace UnitTests.Core
+{
+    public class Calculator
+    {
+
+        public int Add(int x = 0 , int y = 0)
+        {
+            return x + y;
+        }
+    }
+}
